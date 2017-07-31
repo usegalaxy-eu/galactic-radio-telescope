7e253557aa04696992a02e16975f5c094de6e3eb
E Rasche <hxr@hx42.org>
2017-07-31 16:33:59 +0200

    new tables and such

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import GalaxyInstance, Job, Tool, ToolVersion
+from .models import GalaxyInstance, Job, Tool, ToolVersion, JobParam, Metric
 
-admin.site.register(GalaxyInstance)
-admin.site.register(Job)
 admin.site.register(Tool)
 admin.site.register(ToolVersion)
+admin.site.register(GalaxyInstance)
+admin.site.register(Metric)
+admin.site.register(JobParam)
+admin.site.register(Job)
