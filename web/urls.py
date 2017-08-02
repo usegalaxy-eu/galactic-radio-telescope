6b2007ea88fdb23cfa9120a3f7af9d602b368670
E Rasche <hxr@hx42.org>
2017-08-02 19:14:05 +0200

    mostly working

--- web/urls.py
+++ web/urls.py
@@ -1,9 +1,24 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from django.contrib import admin
+from .views import \
+    GalaxyInstanceView, \
+    GalaxyInstanceEdit, \
+    GalaxyInstanceCreate, \
+    GalaxyInstanceCreateSuccess, \
+    GalaxyInstanceListView, \
+    OwnedGalaxyInstanceListView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='base/home.html'), name='home'),
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+
+    url(r'^galaxy/$', GalaxyInstanceListView.as_view(), name='galaxy-instance-list'),
+    url(r'^galaxy/create$', GalaxyInstanceCreate.as_view(), name='galaxy-instance-create'),
+    url(r'^galaxy/owned$', OwnedGalaxyInstanceListView.as_view(), name='owned-galaxy-instance-list'),
+
+    url(r'^galaxy/(?P<slug>[0-9a-f-]{36})/$', GalaxyInstanceView.as_view(), name='galaxy-instance-detail'),
+    url(r'^galaxy/(?P<slug>[0-9a-f-]{36})/edit$', GalaxyInstanceEdit.as_view(), name='galaxy-instance-edit'),
+    url(r'^galaxy/(?P<slug>[0-9a-f-]{36})/success$', GalaxyInstanceCreateSuccess.as_view(), name='galaxy-instance-create-success'),
 ]
