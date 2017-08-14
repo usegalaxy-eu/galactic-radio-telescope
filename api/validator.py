a1306e60baeab9303b6aaec3fb6815aad33c6f88
E Rasche <hxr@hx42.org>
2017-08-14 11:45:13 +0200

    validate better, do not require properties there

--- api/validator.py
+++ api/validator.py
@@ -20,19 +20,16 @@ SCHEMA_V1 = {
       "type": "string"
     },
     "jobs": {
-      "required": [
-        "error",
-        "ok"
-      ],
       "properties": {
-        "error": {
-          "minimum": 0,
-          "type": "integer"
-        },
-        "ok": {
-          "minimum": 0,
-          "type": "integer"
-        },
+        "deleted":{"minimum": 0, "type": "integer"},
+        "deleted_new":{"minimum": 0, "type": "integer"},
+        "error":{"minimum": 0, "type": "integer"},
+        "new":{"minimum": 0, "type": "integer"},
+        "ok":{"minimum": 0, "type": "integer"},
+        "paused":{"minimum": 0, "type": "integer"},
+        "queued":{"minimum": 0, "type": "integer"},
+        "running":{"minimum": 0, "type": "integer"},
+        "waiting":{"minimum": 0, "type": "integer"}
       },
       "type": "object"
     },
